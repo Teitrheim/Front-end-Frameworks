a2582e285b5da2b34b5b48345d8108187a74b9e9
Thomas Eitrheim <teitrheim@gmail.com>
2024-05-23 10:37:18 +0200

    render images

--- src/components/ProductCard/ProductCard.js
+++ src/components/ProductCard/ProductCard.js
@@ -1,29 +1,38 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import styles from "./ProductCard.module.css";
 
 const ProductCard = ({ product }) => {
   return (
-    <div className={styles.card}>
-      <img src={product.image} alt={product.title} className={styles.image} />
-      <div className={styles.info}>
-        <h3>{product.title}</h3>
-        <p>{product.description}</p>
-        <div className={styles.pricing}>
-          <span
-            className={
-              product.discountedPrice < product.price ? styles.oldPrice : ""
-            }
-          >
-            ${product.price}
-          </span>
-          {product.discountedPrice < product.price && (
-            <span className={styles.discountedPrice}>
-              ${product.discountedPrice}
+    <Link to={`/product/${product.id}`} className={styles.cardLink}>
+      {" "}
+      {}
+      <div className={styles.card}>
+        <img
+          src={product.image.url}
+          alt={product.title.alt}
+          className={styles.image}
+        />
+        <div className={styles.info}>
+          <h3>{product.title}</h3>
+          <p>{product.description}</p>
+          <div className={styles.pricing}>
+            <span
+              className={
+                product.discountedPrice < product.price ? styles.oldPrice : ""
+              }
+            >
+              ${product.price}
             </span>
-          )}
+            {product.discountedPrice < product.price && (
+              <span className={styles.discountedPrice}>
+                ${product.discountedPrice}
+              </span>
+            )}
+          </div>
         </div>
       </div>
-    </div>
+    </Link>
   );
 };
 
