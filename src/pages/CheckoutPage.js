0364531ad9dc11a9a690266e0a8ddc2b4e812197
Thomas Eitrheim <teitrheim@gmail.com>
2024-05-24 19:33:13 +0200

    created success page

--- src/pages/CheckoutPage.js
+++ src/pages/CheckoutPage.js
@@ -1,48 +1,46 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 import { useCart } from "../components/CartContext/CartContext";
 import styles from "./CheckoutPage.module.css";
 
 const CheckoutPage = () => {
   const { cartItems, clearCart } = useCart();
+  const navigate = useNavigate();
 
-  // Calculate the total cost of items in the cart
   const total = cartItems.reduce(
     (acc, item) => acc + (item.discountedPrice || item.price),
     0
   );
 
+  const handleCheckout = () => {
+    alert("Proceeding to Checkout...");
+    clearCart();
+    navigate("/success");
+  };
+
   return (
     <div className={styles.checkoutPage}>
       <h1>Cart Summary</h1>
-      <ul className={styles.productList}>
-        {cartItems.map((item) => (
-          <li key={item.id} className={styles.productItem}>
-            <img
-              src={item.image.url}
-              alt={item.title}
-              className={styles.productImage}
-            />
-            <div>
-              <h2>{item.title}</h2>
-              <p>{item.description}</p>
-              <p>Price: ${item.discountedPrice || item.price}</p>
-            </div>
-          </li>
-        ))}
-      </ul>
+      {cartItems.map((item) => (
+        <div key={item.id} className={styles.productItem}>
+          <img
+            src={item.image.url}
+            alt={item.title}
+            className={styles.productImage}
+          />
+          <div>
+            <h2>{item.title}</h2>
+            <p>{item.description}</p>
+            <p>Price: ${item.discountedPrice || item.price}</p>
+          </div>
+        </div>
+      ))}
       <div className={styles.total}>
         <h3>Total: ${total.toFixed(2)}</h3>
       </div>
-      <button
-        className={styles.checkoutButton}
-        onClick={() => alert("Proceeding to Checkout...")}
-      >
+      <button className={styles.checkoutButton} onClick={handleCheckout}>
         Checkout
       </button>
-      <button className={styles.clearCartButton} onClick={clearCart}>
-        Clear Cart
-      </button>{" "}
-      {}
     </div>
   );
 };
