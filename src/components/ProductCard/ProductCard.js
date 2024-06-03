de2bd1f257f188cb0504a0b6e61e06521bbbcc49
Thomas Eitrheim <teitrheim@gmail.com>
2024-06-03 13:34:03 +0200

    Update ProductCard to use react-router-dom's useNavigate hook and improve card click functionality

--- src/components/ProductCard/ProductCard.js
+++ src/components/ProductCard/ProductCard.js
@@ -1,38 +1,46 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import styles from "./ProductCard.module.css";
 
 const ProductCard = ({ product }) => {
+  const navigate = useNavigate();
+
+  const handleCardClick = () => {
+    navigate(`/product/${product.id}`);
+  };
+
   return (
-    <Link to={`/product/${product.id}`} className={styles.cardLink}>
-      {" "}
-      {}
-      <div className={styles.card}>
-        <img
-          src={product.image.url}
-          alt={product.title.alt}
-          className={styles.image}
-        />
-        <div className={styles.info}>
-          <h3>{product.title}</h3>
-          <p>{product.description}</p>
-          <div className={styles.pricing}>
-            <span
-              className={
-                product.discountedPrice < product.price ? styles.oldPrice : ""
-              }
-            >
-              ${product.price}
+    <div className={styles.card} onClick={handleCardClick}>
+      <img
+        src={product.image.url}
+        alt={product.title.alt}
+        className={styles.image}
+      />
+      <div className={styles.info}>
+        <h3>{product.title}</h3>
+        <p>{product.description}</p>
+        <div className={styles.pricing}>
+          <span
+            className={
+              product.discountedPrice < product.price ? styles.oldPrice : ""
+            }
+          >
+            ${product.price}
+          </span>
+          {product.discountedPrice < product.price && (
+            <span className={styles.discountedPrice}>
+              ${product.discountedPrice}
             </span>
-            {product.discountedPrice < product.price && (
-              <span className={styles.discountedPrice}>
-                ${product.discountedPrice}
-              </span>
-            )}
-          </div>
+          )}
         </div>
+        <Link
+          to={`/product/${product.id}`}
+          className={styles.viewProductButton}
+        >
+          View product
+        </Link>
       </div>
-    </Link>
+    </div>
   );
 };
 
